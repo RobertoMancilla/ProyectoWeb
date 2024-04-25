13c07ce8a16e7ee777361b255b70dc9bf9cedc48
Vsebas21 <152416474+Vsebas21@users.noreply.github.com>
2024-04-25 11:48:59 -0600

    Create router.js

---
+++ app/controllers/router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+//const productRouter = require('../routes/products');
+const adminProductRouter = require('../routes/Admin_routes');
+
+const router = express.Router();
+
+//router.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../views/home.html')));
+//router.get('/home', (req, res) => res.sendFile(path.resolve(__dirname, '../views/home.html')));
+//router.get('/shopping_cart', (req, res) => res.sendFile(path.resolve(__dirname, '../views/shopping_cart.html')));
+router.get('/admin', (req, res) => res.sendFile(path.resolve(__dirname, '../views/admin.html')));
+module.exports = router;
