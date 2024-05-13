77286526489b2f2ba1088ef7862f880f26ce2d86
RobertMan <mancillarober24@gmail.com>
2024-05-13 01:20:16 -0600

    ahi va quedando poco a poco

--- app/routes/user-routes.js
+++ app/routes/user-routes.js
@@ -1,12 +1,25 @@
-const router = require("express").Router()
-const Users = require("../data/schema_users")
+const router = require("express").Router();
+const Users = require("../data/schema_users");
 const { verifyAuthToken } = require('../middleware/tokens');
 
-
-router.get('/info', (req, res) => {
-    
+// Get info of the user
+router.get('/info', verifyAuthToken, async (req, res) => {
+    try {
+        const user = await Users.findById(req.id); // asumiendo que req.id se establece en verifyAuthToken
+        if (!user) {
+            return res.status(404).send({ error: 'User not found' });
+        }
+        res.send({
+            email: user.email,
+            name: user.name,
+            surname: user.surname
+        });
+    } catch (error) {
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
 });
+
 // Modify user data
-// router.put('/', userController.updateUser);
+router.put('/', userController.updateUser);
 
 module.exports = router;
